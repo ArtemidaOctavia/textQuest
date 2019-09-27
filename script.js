90a3e93735ef01a4b0e6679b5a820a77d62f1eeb
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2019-09-27 19:10:17 +0300

    new action system

--- script.js
+++ script.js
@@ -1,124 +1,205 @@
 const description = document.querySelector('.description');
-const image = document.querySelector('.image');
 const options = document.querySelector('.options');
 const inventory = document.querySelector('.inventory');
-const illustrationHolder = document.querySelector('.illustration_holder')
+const picture = document.querySelector('.illustration_holder')
 var inventoryArray = [];
 const obj = {
         // scene name = id //
         bedScene: {
-            description: 'А не прогуляться бы по этому беспределу?',
+            description: 'А не прогуляться ли по этому беспределу?',
             image: "pictures/bed.jpg",
             actions: {
-                'Встать': 'roomScene',
-                'Пошариться': 'dualsScene'
+                standUp: {
+                    actionDescription: "Встать",
+                    way: "roomScene",
+                },
+                lookAround: {
+                    actionDescription: "Пошариться",
+                    way: "dualsScene",
+                    receiveItems: {
+                        'Коронный': 'crown',
+                        'Похоронный': 'funeral'
+                    }
+                }
             }
         },
         dualsScene: {
-            items: {
-                'Коронный' : 'crown',
-                'Похоронный' : 'funeral'
-            },
             description: 'Две штуки',
             image: "pictures/duals.jpg",
             actions: {
-                'Назад': 'bedScene'
+                back: {
+                    actionDescription: "Назад",
+                    way: "bedScene",
+                }
             }
         },
         roomScene: {
             description: 'Или не прогуляться?',
             image: 'pictures/room.jpg',
             actions: {
-                'Лечь обратно' : 'bedScene',
-                'Выйти из комнаты' : 'hallScene'
+                lieDown: {
+                    actionDescription: "Лечь обратно",
+                    way: "bedScene"
+                },
+                goOutside: {
+                    actionDescription: "Выйти из комнаты",
+                    way: "hallScene"
+                }
             }
         },
         hallScene: {
-            description: 'Темновато, но я ебал свет включать, чтобы фоткать',
+            description: 'Темновато, но я в лом свет включать, чтобы фоткать',
             image: 'pictures/hall.jpg',
             actions: {
-                'Назад': 'roomScene',
-                'В комнату слева': 'batyaScene',
-                'В комнату справа': 'corridorScene'
+                back: {
+                    actionDescription: "Назад",
+                    way: "roomScene"
+                },
+                left: {
+                    actionDescription: "В комнату слева",
+                    way: "batyaScene"
+                },
+                right: {
+                    actionDescription: "В комнату справа",
+                    way: "corridorScene"
+                }
             }
         },
         batyaScene: {
             description: 'Туда только если с дуалами',
             image: 'pictures/batya.jpg',
             actions: {
-                'Стремительно ретироваться' : 'hallScene',
-            },
-            requiredItems: ['Похоронный', 'Коронный'],
-            specialActions: {
-                'Войти' : 'youDiedScene'
+                back: {
+                    actionDescription: "Стремительно ретироваться",
+                    way: 'hallScene'
+                },
+                enter: {
+                    actionDescription: "Войти",
+                    way: "youDiedScene",
+                    requiredItems: ['Похоронный', 'Коронный'],
+                    lossItems: {
+                        'Коронный': 'crown',
+                        'Похоронный': 'funeral'
+                    },
+                }
             }
         },
         youDiedScene: {
             description: 'АГРХРАХРАХРАХХХХХГГХХ',
             image: 'pictures/youdied.jpg',
             actions: {
-                'Заново': 'bedScene'
+                again: {
+                    actionDescription: 'Заново',
+                    way: 'bedScene',
+                }
             },
-            itemsLoss : {
-                'Коронный' : 'crown',
-                'Похоронный' : 'funeral'
-            }
         },
         corridorScene: {
-            description: 'Ох и наебался я, чтобы это говнище заработало',
+            description: 'Ох и наебался я, чтобы это всё заработало',
             image: 'pictures/corridor.jpg',
             actions: {
-                'Назад' : 'hallScene',
-                'В туалет' : 'morrowindScene',
-                'На кухню' : 'kitchenScene'
-            }
+                back : {
+                    actionDescription: 'Назад',
+                    way: 'hallScene'
+                },
+                toilet : {
+                    actionDescription: 'В туалет',
+                    way: 'morrowindScene'
+                },
+                kitchen : {
+                    actionDescription: 'На кухню',
+                    way: 'kitchenScene'
+                }
+            },
         },
         morrowindScene: {
             description: 'Не, сюда, наверное, не надо',
             image: 'pictures/morrowind.jpg',
             actions: {
-                'Назад' : 'corridorScene'
+                back: {
+                    actionDescription: 'Назад',
+                    way: 'corridorScene'
+                }
             }
         },
         kitchenScene: {
             description: 'О боже, да тут же кошечка!',
             image: 'pictures/kitchen.jpg',
             actions: {
-                'Назад': 'corridorScene',
-                'Приласкать :3' : 'pettingScene'
+                back: {
+                    actionDescription: 'Назад',
+                    way: 'corridorScene'
+                },
+                pet : {
+                    actionDescription: 'Приласкать :3',
+                    way: 'pettingScene'
+                }
             }
         },
         pettingScene: {
             description: 'Кошечка в замешательстве, пора ливать писать код',
             image: 'pictures/pet_catze.jpg',
             actions: {
-                'Оуу :3' : 'kitchenScene'
+                getCuted: {
+                    actionDescription: 'Оуу :3',
+                    way: 'kitchenScene'
+                }
+            }
+        },
+        testScene: {
+            description: 'test',
+            image: 'pictures/pet_catze.jpg',
+            actions: {
+                doThing: {
+                    actionDescription: 'Углубиться',
+                    way: 'pettingScene',
+                    requiredItems: ['Разум'],
+                    lossItems: {
+                        'Разум' : 'mind'
+                    },
+                    receiveItems: {
+                        'Расстройство' : 'disorder'
+                    }
+
+                },
+                doAnotherThing: {
+                    actionDescription: 'Преисполниться',
+                    way: 'testScene',
+                    receiveItems: {
+                        'Разум' : 'mind'
+                    }
+                }
             }
         }
+
 };
 
-const renderOption = function (text, way) {
+const renderOption = function (action) {
+    if (action.requiredItems) {
+        var hasNecessaryItems = true;
+        const requiredItems = action.requiredItems;
+        requiredItems.forEach(function (item) {
+            if (inventoryArray.indexOf(item) === -1) {
+                hasNecessaryItems = false
+            }
+        });
+        if (hasNecessaryItems === false) {
+            return
+        }
+    }
     const option = document.createElement('li');
-    option.textContent = text;
+    option.textContent = action.actionDescription;
     option.classList.add('option');
     option.addEventListener('click', function(){
-        renderScene(way);
+        for (item in action.receiveItems) {
+            giveItems(item, action.receiveItems[item]);
+        }
+        for (item in action.lossItems) {
+            takeAwayItems(item, action.lossItems[item]);
+        }
+        renderScene(action.way);
     });
     options.appendChild(option);
-};
-
-const renderSpecialOption = function (text, way, scene) {
-    var hasNecessaryItems = true;
-    const requiredItems = scene.requiredItems;
-    requiredItems.forEach(function (item) {
-        if (inventoryArray.indexOf(item) === -1) {
-            hasNecessaryItems = false
-        }
-    });
-    if (hasNecessaryItems === false) {
-        return
-    }
-    renderOption(text, way)
 };
 
 const killChildren = function (parent) {
@@ -128,11 +209,11 @@ const killChildren = function (parent) {
 };
 
 const renderPicture = function (scene) {
-    killChildren(illustrationHolder);
+    killChildren(picture);
     var image = document.createElement('img');
     image.classList.add('image');
     image.src = scene.image;
-    illustrationHolder.appendChild(image);
+    picture.appendChild(image);
 };
 
 const giveItems = function (itemName, id) {
@@ -164,16 +245,7 @@ const renderScene = function (id){
     renderPicture(scene);
     killChildren(options);
     for (var option in scene.actions) {
-        renderOption(option, scene.actions[option]);
-    }
-    for (var option in scene.specialActions) {
-        renderSpecialOption(option, scene.specialActions[option], scene);
-    }
-    for (var item in scene.items) {
-        giveItems(item, scene.items[item])
-    }
-    for (var item in scene.itemsLoss) {
-        takeAwayItems(item, scene.itemsLoss[item])
+        renderOption(scene.actions[option]);
     }
 };
 
