c8ee0e94e318940668f4b6f2d16aca9c4015b75b
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2019-11-15 15:26:46 +0300

    refactored killPlayerIfExhausted

--- src/utility/killPlayerIfExhausted.js
+++ src/utility/killPlayerIfExhausted.js
@@ -2,11 +2,10 @@ import { playerStatus, statusLethalValues } from '../content/playerStatus';
 import { renderScene } from './renderScene';
 
 const killPlayerIfExhausted = () => {
-  Object.keys(statusLethalValues).some((key) => {
-    if (playerStatus[key] >= statusLethalValues[key]) {
-      renderScene('youDiedScene');
-    }
-  });
+  if (
+    Object.keys(statusLethalValues).some((key) =>
+    playerStatus[key] >= statusLethalValues[key])
+  ) { renderScene('youDiedScene'); }
 };
 
 export { killPlayerIfExhausted };
